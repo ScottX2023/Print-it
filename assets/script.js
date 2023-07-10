18740abbf27c85cb55075ad431144e03620b87f6
ScottX2023 <131269450+ScottX2023@users.noreply.github.com>
2023-07-10 23:33:44 +0200

    update slide

--- assets/script.js
+++ assets/script.js
@@ -1,79 +1,68 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    "image": "slide1.jpg",
+    "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
+  },
+  {
+    "image": "slide2.jpg",
+    "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+  },
+  {
+    "image": "slide3.jpg",
+    "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+  },
+  {
+    "image": "slide4.png",
+    "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+  }
+];
 
 const arrowLeft = document.querySelector('.arrow_left');
-
-
-arrowLeft.addEventListener('click', function() {
-  console.log("Left arrow click");
-});
-
-
 const arrowRight = document.querySelector('.arrow_right');
-
-
-arrowRight.addEventListener('click', function() {
-  console.log("Right arrow click");
-});
-
-
 const dots = document.querySelectorAll('.dot');
-
-function updateActiveDot() {
-  dots.forEach((dot, index) => {
-    if (index === currentIndex) {
-      dot.classList.add('dot_selected');
-    } 
-	
-	else {
-      dot.classList.remove('dot_selected');
-    }
-  });
-}
-
 const slideImage = document.querySelector('.banner-img');
 const totalSlides = slides.length;
+const dotsContainer = document.querySelector('.dots');
 let currentIndex = 0;
 
-arrowLeft.addEventListener('click', function() {
-  currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
-  updateSlide();
-  updateActiveDot();
-});
-
-arrowRight.addEventListener('click', function() {
-  currentIndex = (currentIndex + 1 + totalSlides) % totalSlides;
-  updateSlide();
-  updateActiveDot();
-});
+updateSlide();
+updateActiveDot();
 
 function updateSlide() {
   const slide = slides[currentIndex];
   slideImage.src = "./assets/images/slideshow/" + slide.image;
+  const tagLineElement = document.querySelector('#banner > p');
+  tagLineElement.innerHTML = slide.tagLine;
 }
 
 function updateActiveDot() {
-  dots.forEach((dot, index) => {
-    if (index === currentIndex) {
+  dotsContainer.innerHTML = '';
+  for (let i = 0; i < totalSlides; i++) {
+    const dot = document.createElement('span');
+    dot.classList.add('dot');
+    if (i === currentIndex) {
       dot.classList.add('dot_selected');
-    } else {
-      dot.classList.remove('dot_selected');
     }
-  });
+    dotsContainer.appendChild(dot);
+  }
 }
+
+arrowLeft.addEventListener('click', function() {
+  currentIndex--; 
+  if (currentIndex < 0){
+     currentIndex = totalSlides - 1
+    }
+  updateSlide();
+  updateActiveDot();
+});
+
+arrowRight.addEventListener('click', function() {
+  currentIndex++;
+  if (currentIndex === totalSlides){
+    currentIndex = 0
+  }
+  updateSlide();
+  updateActiveDot();
+});
+
+
